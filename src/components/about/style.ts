2e49b0efe224c595b0548e5a3d09413245ac6006
gustavoventieri <ventierigustavo@gmail.com>
2025-03-14 05:37:30 -0300

    feat: about added

---
+++ src/components/about/style.ts
@@ -0,0 +1,72 @@
+import { Dimensions, StyleSheet } from "react-native";
+
+export const { width } = Dimensions.get("window");
+
+
+export const styles = StyleSheet.create({
+	
+	container: {
+		padding: 10,
+	  }
+	,imageSessaoInfo: {
+		width: "30%", 
+		height: undefined,
+		aspectRatio: 1.3,
+		marginLeft: 10,
+	},
+	modalContainer: {
+		flex: 1,
+		backgroundColor: "#f3f3ff",
+		marginTop: 0,
+		marginLeft: 0,
+		marginRight: 0,
+		padding: 0,
+	},
+	modalInfo: {
+		flexDirection: "row",
+		justifyContent: "center",
+	},
+	line: {
+		height: 1,
+		width: "100%",
+		backgroundColor: "#ccc",
+		marginBottom: 10,
+		marginTop: 10,
+	
+	},
+	textInfo: {
+	    paddingTop: 0,      
+		paddingRight: 20,  
+		paddingBottom: 20, 
+		paddingLeft: 20,    
+	},
+	textTitle:{
+		fontWeight: "bold",
+		fontSize: width * 0.04, 
+		marginBottom: 10,
+	},
+	itemContainer: {
+		flexDirection: 'row',
+		alignItems: 'flex-start',
+		marginTop: 10,
+	  },
+	  bullet: {
+		fontSize:width * 0.04,
+		lineHeight: 22,
+		marginRight: 10,
+	  },
+	  itemText: {
+		flex: 1,
+		fontSize: width * 0.035,
+		lineHeight: 22,
+		fontWeight: "bold",
+	  },
+	  itemContainerFlat: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		marginBottom: 10,
+		flex: 1, 
+		fontWeight: "bold",
+		
+	  },
+});
