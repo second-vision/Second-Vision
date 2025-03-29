6a2c8ba0d9481d5e1229a81938a474ee7e8c2ac8
gustavoventieri <ventierigustavo@gmail.com>
2025-03-29 15:17:17 -0300

    feat: splash screen added to file handler

--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -3,4 +3,4 @@ export * from "./bluetoothOn/BluetoothOn";
 export * from "./home/Home";
 export * from "./controlBluetooth/ControlBluetooth";
 export * from "./termsOfUse/TermsOfUse";
-
+export * from "./splash/Splash";
