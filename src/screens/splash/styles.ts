23444d844ae3305537aba96851ceab9b2536f988
gustavoventieri <ventierigustavo@gmail.com>
2025-03-29 17:38:59 -0300

    fix: type of style files changed to .ts

---
+++ src/screens/splash/styles.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "black",
+  },
+  video: {
+    width: "100%",
+    height: "100%",
+  },
+});
